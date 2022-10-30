50dbd46529be23dd4c5b6d6d671720141d7b8073
Hioj72 <ivan.ojeda722@gmail.com>
2022-10-30 01:36:01 -0600

    agregamos contenedor
    agregamos contenedor con la documentación sobre trazabilidad

--- app_fabricacion/src/componente1.js
+++ app_fabricacion/src/componente1.js
@@ -11,6 +11,17 @@ export function MostrarProyectos(props) {
             <p className='planos'><strong>Planos:</strong> {props.planos} </p>
             <p className='materiales'><strong>Materiales:</strong> {props.materiales} </p>
           </div>
+          <div className='contenedor_trazabilidad'>
+            <ul><p><strong>Documentación de la obra:</strong></p>
+            <li><a href="#">Formato_trazabilidad_001.zip</a></li>
+            <li><a href="#">Formato_trazabilidad_002.zip</a></li>
+            <li><a href="#">Formato_trazabilidad_003.zip</a></li>
+            <li><a href="#">Listado de liberaciones_001.txt</a></li>
+            <li><a href="#">Listado de liberaciones_002.txt</a></li>
+            <li><a href="#">Listado de liberaciones_003.txt</a></li>
+            </ul> 
+            
+          </div>
           
                       
         </div>
